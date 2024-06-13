9c83c0e6a24239284094d8e77a43720ff1b53593
mayemba312 <kerenskyhappuc@gmail.com>
2024-06-13 18:11:40 +0100

    Revert "rajout de bouton dans la table vehicule et chauffeur qui etaient des liens"
    This reverts commit b0a1f8eae58056b4603660bdabfa910d38947768.

--- resources/views/chauffeurs/index.blade.php
+++ resources/views/chauffeurs/index.blade.php
@@ -6,7 +6,6 @@
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
                 {{ __('Liste des chauffeurs') }}
             </h2>
-            
             <form class="flex items-center max-w-sm  mr-4 my-4 " method="GET" action="{{route('vehicules.search')}}">
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
@@ -48,24 +47,6 @@
         <div class="tooltip-arrow" data-popper-arrow></div>
     </div>  
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-4">
-
-                @if (session('success'))
-            <div id="alert-3" class="flex items-center p-4 mb-4 text-orange-800 rounded-lg bg-orange-100 dark:bg-gray-800 dark:text-orange-400" role="alert">
-                <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                </svg>
-                <span class="sr-only">Alert</span>
-                <div class="ms-3 text-sm font-medium">
-                    {{ session('success') }}
-                </div>
-                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-orange-500 rounded-lg focus:ring-2 focus:ring-orange-400 p-1.5 hover:bg-orange-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-orange-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-3" aria-label="Close">
-                  <span class="sr-only">Close</span>
-                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                  </svg>
-                </button>
-              </div>
-            @endif
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -87,7 +68,7 @@
                                 <td class="px-6 py-4">
                                      <a onclick="supprimer(event);" data-modal-target="delete-modal"
                                     data-modal-toggle="delete-modal" href="{{ route('chauffeurs.destroy', $chauffeur->id) }}"
-                                    class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xs px-5 py-2.5 text-center me-2 mb-2">Supprimer</a>
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Supprimer</a>
                                 </td>
                                
                             </tr>
