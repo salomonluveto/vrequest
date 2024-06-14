e456e08bf681b0401641a45adb61ea443a37057a
maria bolikango <bolikangomaria@gmail.com>
2024-06-14 13:58:58 +0100

    Création du model de la table site

---
+++ app/Models/Site.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Site extends Model
+{
+    use HasFactory;
+
+    public function demandes(){
+        return $this->hasMany(Demande::class,'site_id', 'id');
+    }
+}
