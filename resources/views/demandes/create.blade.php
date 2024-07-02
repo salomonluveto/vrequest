2226cbccdb994418dc140532d5caf6d176507c79
Mazamoon1504 <nsholeobed@gmail.com>
2024-07-02 14:53:51 +0100

    create

--- resources/views/demandes/create.blade.php
+++ resources/views/demandes/create.blade.php
@@ -152,10 +152,10 @@
                 </div>
             </div>
             
-            <a href="{{route('demandes.index')}}"
-                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-                    Demander une course
-            </a>
+                <button
+                    class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                        Demander une course
+                </button>
         </form>
         <script src="https://unpkg.com/jquery/dist/jquery.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
