995647ba9cd4c9d3524127c06ff76969fc45abe5
maria bolikango <bolikangomaria@gmail.com>
2024-07-03 10:21:30 +0100

    Ajout de l'ecouteur d'événement pour le champ nombre de passagers

--- resources/views/demandes/create.blade.php
+++ resources/views/demandes/create.blade.php
@@ -151,10 +151,10 @@
                     </div>
                 </div>
             </div>
-            
+
             <button
                 class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-                    Demander une course
+                Demander une course
             </button>
         </form>
         <script src="https://unpkg.com/jquery/dist/jquery.min.js"></script>
@@ -167,5 +167,17 @@
     <x-destinationCarte />
     <x-destinationListe />
     <x-styleAutocomplete />
+    <script>
+        // Ajoutez un écouteur d'événement pour le champ "Nombre de passagers"
+        const passengerInput = document.getElementById('price');
+        passengerInput.addEventListener('input', () => {
+            const value = parseInt(passengerInput.value);
+            if (value < 1 || value > 20) {
+                passengerInput.setCustomValidity('Le nombre de passagers doit être compris entre 1 et 20.');
+            } else {
+                passengerInput.setCustomValidity('');
+            }
+        });
+    </script>
 
 </x-app-layout>
