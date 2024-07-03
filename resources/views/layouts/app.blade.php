a2b525c440ae4bad607e3e5327bfb9d659f8dc24
maria bolikango <bolikangomaria@gmail.com>
2024-07-03 10:17:18 +0100

    cdn dataTables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,15 @@
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
 
+    <!-- Link data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css">
+    
     <!-- jquery-->
     <link rel="stylesheet" href="jquery.typeahead.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+     <!-- scripts data Tables -->
+     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
 
     <!--scripts openStreetmap-->
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
