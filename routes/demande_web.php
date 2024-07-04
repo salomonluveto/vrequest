6f668331a692b5b000905e2c08916c2ceccefed4
maria bolikango <bolikangomaria@gmail.com>
2024-07-04 16:36:01 +0100

    ajout de la route pour les sites

--- routes/demande_web.php
+++ routes/demande_web.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/site/{nom}', [SiteController::class, 'getCoordinates']);
 Route::resource('/demandes', DemandeController::class);
+Route::post('/sites', [SiteController::class, 'store'])->name('sites');
+Route::get('/site', [SiteController::class,'index'])->name('site');
 Route::get('/envoyermailmanager',[DemandeController::class,'envoyerMailManager'])->name('envoyermailmanager');
-Route::get('/envoyermailauchefcharroi',[DemandeController::class,'envoyerMailAuChefCharroi'])->name('envoyermailauchefcharroi');
+Route::get('/envoyermailauchefcharroi/{id}',[DemandeController::class,'envoyerMailAuChefCharroi'])->name('envoyermailauchefcharroi');
