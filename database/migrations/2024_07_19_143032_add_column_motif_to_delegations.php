5fbc09af4b056bc3bd8f59fee6f30852a8116fcf
Mazamoon1504 <nsholeobed@gmail.com>
2024-07-24 16:29:05 +0100

    Migrations sur le motif de la délégation

--- database/migrations/2024_07_19_143032_add_column_motif_to_delegations.php
+++ database/migrations/2024_07_19_143032_add_column_motif_to_delegations.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('demandes', function (Blueprint $table) {
-            $table->string('raison')->default("null");
-        });
+        Schema::table('delegations', function (Blueprint $table) {
+            $table->string('motif');        
+         });
     }
 
     /**
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('demandes', function (Blueprint $table) {
-            $table->dropColumn(['raison']);
+        Schema::table('delegations', function (Blueprint $table) {
+            //
         });
     }
 };
