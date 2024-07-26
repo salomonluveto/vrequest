6c9485c8ea8850b99f6671c146dec6bf0a6fe387
maria bolikango <bolikangomaria@gmail.com>
2024-07-26 18:12:06 +0100

    création de la route pour la récuperation des demandes par utilisateurs

--- routes/apiDemande.php
+++ routes/apiDemande.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\ApiDemandeController;
 Route::apiResource('/demandes', ApiDemandeController::class);
 Route::post('user-demande',[ApiDemandeController::class,'userDemande']);
 Route::post('last-demande',[ApiDemandeController::class,'lastDemande']);
+Route::post('getdemande',[ApiDemandeController::class,'getdemande']);
